135bf9b68bfd6669cce989230e4cfabd5ad4399d
Rohit Raikhy <raikhy101@gmail.com>
2020-11-20 21:07:23 -0500

    updated for heroku

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import LoginComponent from "./Components/LoginComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
-import RegisterComponent from "./Components/RegisterComponent";
+// import RegisterComponent from "./Components/RegisterComponent";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import loginReducer from "./reducers/LoginReducer";
 import registerReducer from "./reducers/RegisterReducer";
 import userReducer from "./reducers/UserReducer"
-import HomeComponent from "./Components/HomeComponent";
-import HomePage from "./Components/HomePageComponents/HomePage";
-import Home from "./Components/HomePageComponents/RouterManagerComponent";
+// import HomeComponent from "./Components/HomeComponent";
+// import HomePage from "./Components/HomePageComponents/HomePage";
+// import Home from "./Components/HomePageComponents/RouterManagerComponent";
 import RouterManagerComponent from "./Components/HomePageComponents/RouterManagerComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css"
