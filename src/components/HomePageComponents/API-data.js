3c4b53cfda3f0f103ade368b7f3547d38e424f81
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 13:23:16 +0100

    Update API-data.js

--- src/components/HomePageComponents/API-data.js
+++ src/components/HomePageComponents/API-data.js
@@ -1,8 +1,1 @@
 
-
-// export const APP_ID = '9b948587';
-// export const APP_KEY = '2948aa6ee2af4b31b23255c07053cb91'
-// export const API_KEY = "3e7d74407131b5861669fbea1d249b1d"
-// export consURL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2`;
-//
-// export default {APP_ID, APP_KEY, URL}
