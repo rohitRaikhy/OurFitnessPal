1d5bbaa7545b537fe458351051c71c18c25c44a8
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 14:11:34 +0100

    Update SearchService.js

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -1,8 +1,6 @@
 const SEARCH_INGREDIENT_ITEM = "https://api.spoonacular.com/food/ingredients/search?query=";
 const API_KEY = "apiKey=56048a40e399452eb102658417d73d76";
 const DETAIL_INGREDIENT_ITEM = "https://api.spoonacular.com/food/ingredients";
-
-// https://api.spoonacular.com/food/ingredients/search?query=cheddar%20cheese&sort=calories&sortDirection=desc&apiKey=56048a40e399452eb102658417d73d76
 const searchItem = (itemName) =>
   fetch(`${SEARCH_INGREDIENT_ITEM}${itemName}&sort=calories&sortDirection=desc&${API_KEY}`).then(response => response.json())
 
