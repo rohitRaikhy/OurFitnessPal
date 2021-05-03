848d8a6ba9cddecf91e3cbb00e956603b9163cfd
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 13:57:41 +0100

    Update HomePageCards.js

--- src/components/HomePageComponents/HomePageCards.js
+++ src/components/HomePageComponents/HomePageCards.js
@@ -8,25 +8,14 @@ const HomePageCards = ({search}) => {
 
 return(
     <div>
-        {/*<div className="wbdv-cards">*/}
-        {/*    <div className="cards-container">*/}
-        {/*        <div className="cards-wrapper">*/}
-        {/*        <ul className= "cards-items">*/}
-        {/*            <HomePageCardItems/>*/}
-        {/*        </ul>*/}
-        {/*        </div>*/}
-        {/*    </div>*/}
-        {/*</div>*/}
         <div className="wbdv-cards">
             <div className= "wbdv-cards-container">
                 <div className= "wbdv-cards-wrapper">
                     <div className="wbdv-margin-cards">
                         <ul className="wbdv-margin-cards">
                             <HomePageCardItems
-                            // src = "https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-1200x628-facebook-1200x628.jpg"
                                 src = {image}
                             text = "Food"
-                            // label = "Test"
                                 label = {label}
                             path ="/services"
                             />
