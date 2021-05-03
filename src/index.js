79aec3676f5e9f79e407064d5c0e37ba9fa5d2f2
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 14:12:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,23 +37,8 @@ const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
-      {/*<RegisterComponent/>*/}
-      {/*<SearchComponent/>*/}
-      {/**!/*/}
-        {/*<RegisterComponent/>*/}
-        {/*<HomeComponent/>*/}
         <RouterManagerComponent/>
-        {/*<HomePage/>*/}
     </Provider>,
-    // <React.StrictMode>
-    //   {/*<LoginComponent />*/}
-    //
-    // </React.StrictMode>,
-
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
