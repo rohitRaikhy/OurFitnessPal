e9dadd89ad9c45bd538402a7b40d05865c53437c
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 13:17:33 +0100

    Update RouteAction.js

--- src/actions/RouteAction.js
+++ src/actions/RouteAction.js
@@ -2,11 +2,6 @@ import {useHistory} from "react-router";
 import {Redirect} from 'react-router-dom';
 import React from "react";
 
-// export function RouteToLogin() {
-//   let history = useHistory();
-//   history.push("/ourfitnesspal/login");
-// }
-
 export const RouteToLogin = () => {
   return  <Redirect  to="/ourfitnesspal/login" />
 }
