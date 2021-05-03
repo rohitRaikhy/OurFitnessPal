c0231174d8bad7d011bd8e4715af9fc655825133
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 14:10:18 +0100

    Update HomeService.js

--- src/services/HomeService.js
+++ src/services/HomeService.js
@@ -1,6 +1,3 @@
-//TODO: changed for heroku db
-// const USER_URL = "http://localhost:8080/ourfitnesspal/users";
-
 const USER_URL = "https://ourfitnesspalserver.herokuapp.com/ourfitnesspal/users"
 
 export const findAllUsers = () =>
