1d08834035dace907f622835ddde6b72f887bbde
Rohit Raikhy <raikhy101@gmail.com>
2021-05-03 13:19:26 +0100

    Update SearchComponent.js

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,10 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
 import { Link } from "react-router-dom";
-// import {
-//   onChangeItemName, searchItem
-// } from "../actions/SearchAction";
-
 import {onChangeItemName, searchItem} from "../actions/SearchAction";
 import searchReducer from "../reducers/SearchReducer";
 import ResultItemComponent from "./ResultItemComponent";
@@ -41,11 +37,7 @@ class SearchComponent extends React.Component {
     }
 
     searchByTitle = (itemName) => {
-        // this.props.history.push(`/ourfitnesspal/home/${this.props.keyword}`)
-        // {console.log("these are the props", this.props.history)}
-        // alert(itemName)
         this.props.history.push(`/ourfitnesspal/search/${itemName}`)
-        // history.push(`/ourfitnesspal/home/${keyword}`)
     }
 
     render() {
@@ -71,10 +63,6 @@ class SearchComponent extends React.Component {
                 className="btn btn-primary">
                 Search
             </button>
-          {/*<div className="col-sm-2">*/}
-          {/*  <button className="btn btn-primary btn-block"*/}
-          {/*          onClick={() => this.searchItem(this.state.itemName)}>Search</button>*/}
-          {/*</div>*/}
         </div>
       </form>
       {this.props.itemName}
@@ -99,7 +87,6 @@ class SearchComponent extends React.Component {
             </tr>
           )}
       </table>
-      {/*{results.map((result) => <li>{result.fdcId} {result.description} {result.ingredients}</li>)}*/}
     </div>
     </div>
         )
@@ -111,95 +98,8 @@ const stateToPropertyMapper = (state) => ({
   results: state.searchReducer.results,
 })
 const propertyToDispatchMapper = (dispatch) => ({
-  // onChangeItemName : (itemName) => {onChangeItemName(dispatch, itemName); searchItem(dispatch, itemName)},
     onChangeItemName : (itemName) => {onChangeItemName(dispatch, itemName)},
   searchItem : (itemName) => searchItem(dispatch, itemName),
 })
 
 export default withRouter(connect(stateToPropertyMapper, propertyToDispatchMapper) (SearchComponent))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const SearchComponent = ({
-//   itemName, results,
-//   onChangeItemName,searchItem,
-// }) =>
-//     <div className="container">
-//         <HomePageHeader/>
-//         <div className="wbdv-register-page-margins">
-//       <h1>Search Food Nutrition</h1>
-//       <form>
-//         <div className="form-group row">
-//           <label htmlFor="foodNameFld" className="col-sm-2 col-form-label">
-//             FoodName </label>
-//           <div className="col-sm-8">
-//             <input id="foodNameFld"
-//                    className="form-control"
-//                    placeholder="Cheddar Cheese"
-//                    onChange={(event) => onChangeItemName(event.target.value)}/>
-//
-//           </div>
-//             {/*<button*/}
-//             {/*    onClick={() => searchByTitle(itemName)}*/}
-//             {/*    className="btn btn-primary">*/}
-//             {/*    Search*/}
-//             {/*</button>*/}
-//           {/*<div className="col-sm-2">*/}
-//           {/*  <button className="btn btn-primary btn-block"*/}
-//           {/*          onClick={() => this.searchItem(this.state.itemName)}>Search</button>*/}
-//           {/*</div>*/}
-//         </div>
-//       </form>
-//       {itemName}
-//       <h3>RESULT</h3>
-//       <table className="table table-hover">
-//         <thead>
-//           <tr>
-//             <th>id</th>
-//             <th>name</th>
-//             <th>image</th>
-//           </tr>
-//         </thead>
-//         {results && results.map((result) =>
-//             <tr>
-//               <td>{result.id}</td>
-//               <td>
-//                 <Link to={`/ourfitnesspal/search/details/${result.id}`}>
-//                   {result.name}
-//                 </Link>
-//               </td>
-//               <td><img src={`https://spoonacular.com/cdn/ingredients_100x100/${result.image}`}/></td>
-//             </tr>
-//           )}
-//       </table>
-//       {/*{results.map((result) => <li>{result.fdcId} {result.description} {result.ingredients}</li>)}*/}
-//     </div>
-//     </div>
-//
-//
-// const stateToPropertyMapper = (state) => ({
-//   itemName: state.searchReducer.itemName,
-//   results: state.searchReducer.results,
-// })
-// const propertyToDispatchMapper = (dispatch) => ({
-//   onChangeItemName : (itemName) => {onChangeItemName(dispatch, itemName); searchItem(dispatch, itemName)},
-//   searchItem : (itemName) => searchItem(dispatch, itemName),
-//     //TODO: ADDED HERE
-//     // searchByTitle: (itemName) => searchByTitle(dispatch, itemName)
-// })
-//
-// export default connect(stateToPropertyMapper, propertyToDispatchMapper) (SearchComponent)
